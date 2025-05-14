74d7fbc3e4316258b5852a170a39139289c33c18
Biliqis Onikoyi <105673949+BiliqisO@users.noreply.github.com>
2025-05-14 20:22:49 +0100

    decentralized-health test 1

--- tests/decentralized-health.test.ts
+++ tests/decentralized-health.test.ts
@@ -1,21 +1,20 @@
-
-import { describe, expect, it } from "vitest";
+import { ClarityType } from "@stacks/transactions";
+import { principal, none, bool } from "@stacks/transactions/dist/cl";
+import { describe, it, expect } from "vitest";
 
 const accounts = simnet.getAccounts();
-const address1 = accounts.get("wallet_1")!;
+const admin = accounts.get("deployer")!;
+const alice = accounts.get("wallet_1")!;
+const bob = accounts.get("wallet_2")!;
 
-/*
-  The test below is an example. To learn more, read the testing documentation here:
-  https://docs.hiro.so/stacks/clarinet-js-sdk
-*/
-
-describe("example tests", () => {
-  it("ensures simnet is well initialised", () => {
-    expect(simnet.blockHeight).toBeDefined();
+describe("decentralized-health.clar", () => {
+  it("should allow a new user to sign up without a referer", () => {
+    const { result } = simnet.callPublicFn(
+      "decentralized-health",
+      "sign-up",
+      [principal(alice), none()],
+      admin
+    );
+    expect(result).toBeOk(bool(true));
   });
-
-  // it("shows an example", () => {
-  //   const { result } = simnet.callReadOnlyFn("counter", "get-counter", [], address1);
-  //   expect(result).toBeUint(0);
-  // });
 });
